924139de26c45fb299e929fc22d2ced60a88f6dc
Christopher Bartling <chris.bartling@gmail.com>
2022-01-01 12:24:33 -0600

    Created convert function.

--- dna-to-rna-conversion/rust/src/main.rs
+++ dna-to-rna-conversion/rust/src/main.rs
@@ -1,3 +1,20 @@
+fn convert(dna: &str) -> Result<String, String> {
+    Ok(str::replace(dna, "T", "U"))
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_convert() -> Result<(), String> {
+        assert_eq!(convert("CGAT")?, "CGAU");
+        Ok(())
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
