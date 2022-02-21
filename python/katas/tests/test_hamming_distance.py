5a0dc8920bc6e539064e955fe41d14d6fc8ad4f0
Christopher Bartling <chris.bartling@gmail.com>
2022-02-21 14:14:50 -0600

    More parameterized test cases

--- python/katas/tests/test_hamming_distance.py
+++ python/katas/tests/test_hamming_distance.py
@@ -4,8 +4,14 @@ from hamming_distance import compute
 
 
 @pytest.mark.parametrize('dna_string1,dna_string2,expected', [
-    ('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT', 7),
+    ('GAGCCTACTAACGGGAT', 'GAGCCTACTAACGGGAT', 0),
+    ('GAGCCTACTAACGGGAT', 'CAGCCTACTAACGGGAT', 1),
+    ('GAGCCTACTAACGGGAT', 'GACCCAACTAACGGGAT', 2),
+    ('GAGCCTACTAACGGGAT', 'GAGCCATGTAACGGGAT', 3),
+    ('GAGCCTACTAACGGGAT', 'GAGCCTACTTTCGCCAT', 4),
+    ('GAGCCTACTAACGGGAT', 'GAGCCTACTTTCGCCAA', 5),
     ('GAGCCTACTAACGGGAT', 'GATCGTAATGACGGCCT', 6),
+    ('GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT', 7),
 ])
 def test_hamming_distance_computation(dna_string1: str, dna_string2: str, expected: int) -> None:
     assert expected == compute(dna_string1, dna_string2)
