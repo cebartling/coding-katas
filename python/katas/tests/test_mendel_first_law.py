8cf84db0a3a85d1c91b683e75546265f962ae544
Christopher Bartling <chris.bartling@gmail.com>
2022-03-02 12:45:14 -0600

    More test cases

--- python/katas/tests/test_mendel_first_law.py
+++ python/katas/tests/test_mendel_first_law.py
@@ -1,4 +1,5 @@
 import pytest
+from numpy import nan
 
 from mendel_first_law import calculate_probability
 
@@ -7,6 +8,7 @@ from mendel_first_law import calculate_probability
     (2, 2, 2, 0.7833333333333333),
     (4, 4, 4, 0.7651515151515151),
     (10, 10, 10, 0.7557471264367817),
+    (1, 1, 1, 0.8333333333333334),
 ])
 def test_calculate_probability(k: int, m: int, n: int, expected: float):
     assert calculate_probability(k=k, m=m, n=n) == expected
